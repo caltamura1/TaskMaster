ebcc314bbd58b7c90822c8246695ac8683194fd3
Christian Altamura <christiancaltamura@gmail.com>
2024-05-21 12:08:37 -0400

    Initial Commit

---
+++ TaskMaster/TaskMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMasterApp.swift
+//  TaskMaster
+//
+//  Created by Christian Altamura on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
